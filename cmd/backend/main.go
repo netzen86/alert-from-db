129c64ed16c9188090bd3fdf6aa2720c6d54cdba
Dmitrii Novikov <dmitrii@netzen.dev>
2025-02-02 22:22:21 +0500

    added backend main file

---
+++ cmd/backend/main.go
@@ -0,0 +1,47 @@
+package main
+
+// Package main - пакет сервера
+// Приложение для получения и храненния метрик.
+// Приложение позволяет хранить метрики в текстовом файле, ОЗУ и в базе данных.
+
+import (
+	"alert-from-db/internal/backend"
+	"alert-from-db/internal/repository/db"
+	"context"
+	"log"
+	"net"
+	_ "net/http/pprof"
+)
+
+func main() {
+	var err error
+	ctx := context.Background()
+	storage, err := db.NewDBStorage(ctx, db.DBconstring)
+	if err != nil {
+		log.Fatalf("error when creating storage %v ", err)
+	}
+
+	err = backend.MakeDBMigrations(ctx, storage)
+	if err != nil {
+		log.Fatalf("error when making migration %v ", err)
+	}
+
+	// определяем порт для gRPC сервера
+	listen, err := net.Listen(backend.ProtoTCP, backend.EndpointRPC)
+	if err != nil {
+		log.Fatalf("error when setup net listen %v", err)
+	}
+	gSRV, err := backend.GetgRPCSrv(ctx, storage)
+	if err != nil {
+		log.Fatalf("error when gettin gRPC server %v", err)
+	}
+
+	log.Printf("!!! SERVER START at %v %v!!!\n", backend.EndpointRPC, backend.ProtoTCP)
+
+	// запуск gRPC сервера
+	go func() {
+		if err = gSRV.Serve(listen); err != nil {
+			log.Fatalf("error when run gRPC server %v", err)
+		}
+	}()
+}
